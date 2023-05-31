cd6fac85df0260a251aee47a9528bc9d4a408561
Daniel Philipi <daniphilipi379@gmail.com>
2023-05-30 23:26:32 -0300

    Feat: Innitial code and signin e signup codes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
-function App() {
+
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Dashboard from './pages/Dashboard';
+import FillSubscription from './pages/Dashboard/FillSubscription';
+import Payment from './pages/Dashboard/Payment';
+import Hotels from './pages/Dashboard/Hotel';
+import Reservation from './pages/Dashboard/Reservation';
+import Activities from './pages/Dashboard/Activities';
+import Certificate from './pages/Dashboard/Certificate';
+
+import { UserProvider } from './contexts/UserContext';
+
+import useToken from './hooks/useToken';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ToastContainer />
+        <UserProvider>
+          <Router>
+            <Routes>
+              <Route path="/sign-up" element={<SignUp />} />
+              <Route path="/sign-in" element={<SignIn />} />
+
+              <Route
+                path="/"
+                element={
+                  <ProtectedRouteGuard>
+                    <Dashboard />
+                  </ProtectedRouteGuard>
+                }
+              >
+                <Route path="subscription" element={<FillSubscription />} />
+                <Route path="payment" element={<Payment />} />
+                <Route path="hotel" element={<Hotels />} />
+                <Route path="hotel/reservation" element={<Reservation />} />
+                <Route path="activities" element={<Activities />} />
+                <Route path="certificate" element={<Certificate />} />
+                <Route index path="*" element={<Navigate to="/dashboard/subscription" />} />
+              </Route>
+            </Routes>
+          </Router>
+        </UserProvider>
+    </>
   );
 }
 
-export default App;
+function ProtectedRouteGuard({ children }) {
+  const token = useToken();
+
+  if (!token) {
+    return <Navigate to="/sign-in" />;
+  }
+
+  return <>{children}</>;
+}
